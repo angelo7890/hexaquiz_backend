6c54b2b27bb335452cbe4bd50428b1bae01731cb
angelo7890 <ar293978@gmail.com>
2026-04-26 23:21:02 -0300

    first commit

---
+++ src/main/java/com/hexaquiz/HexaquizApplication.java
@@ -0,0 +1,13 @@
+package com.hexaquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexaquiz/HexaquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
